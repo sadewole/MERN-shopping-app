6d440bb1dcc91dc2acfead865277eff2ec911480
Samuel Adewole <samueladewole15@gmail.com>
2019-07-16 07:59:08 +0100

    successfully running react and express together

--- server.js
+++ server.js
@@ -27,6 +27,13 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+})
+
+
 app.use('/api/v1/', items)
 
 
